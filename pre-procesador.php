6a7615dcac10c1a291586c88634fbb144ee11f57
Daniel Pinto <apintod@uninorte.edu.co>
2022-07-20 16:29:35 -0500

    pre-procesador

---
+++ pre-procesador.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP COMO PREPROCESADOR DE HTML</title>
+</head>
+<body>
+    
+    <header>
+
+        <h1>¡Hola esto es HTML desde PHP!</h1>
+
+    </header>
+
+    <?php echo "<p>Ahora esto es PHP 😍.</p> <br>
+    <br><br><br><br><br><br><br><br><br> Otra cosa 🤠" ?>
+
+    
+
+</body>
+</html>
