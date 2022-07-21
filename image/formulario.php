90933935f44c24ba23285a4693cf8fe42c946d73
Daniel Pinto <apintod@uninorte.edu.co>
2022-07-20 19:45:49 -0500

    montar imagenes al server

---
+++ image/formulario.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario por post</title>
+</head>
+<body>
+
+    <form action="./server.php" method="post" enctype="multipart/form-data">
+
+        <label for="nombre">Nombre: </label>
+        <input type="text" name="nombre" id="nombre">
+
+        <label for="image">Foto: </label>
+        <input type="file" name="image" id="image">
+
+        <button type="submit">Enviar formulario</button>
+        
+    </form>
+    
+</body>
+</html>
---
+++ image/server.php
@@ -0,0 +1,12 @@
+<?php 
+
+/* echo "<pre>";
+var_dump($_FILES);
+echo "<pre>";
+ */
+
+$basename = $_FILES["image"]["name"];
+
+echo $basename; 
+
+?>
