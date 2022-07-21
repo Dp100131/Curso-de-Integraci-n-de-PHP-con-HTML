f149852d7a3da0e1bd9520e51930940266813fb5
Daniel Pinto <apintod@uninorte.edu.co>
2022-07-21 01:10:58 -0500

    images

--- image/formulario.php
+++ image/formulario.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulario por post</title>
+    <title>Envio de imagines</title>
 </head>
 <body>
 
@@ -16,7 +16,7 @@
         <label for="image">Foto: </label>
         <input type="file" name="image" id="image">
 
-        <button type="submit">Enviar formulario</button>
+        <button type="submit">Enviar</button>
         
     </form>
     
--- image/server.php
+++ image/server.php
@@ -1,12 +1,28 @@
 <?php 
 
-/* echo "<pre>";
+/* 
+echo "<pre>";
 var_dump($_FILES);
 echo "<pre>";
- */
+*/
 
 $basename = $_FILES["image"]["name"];
+$url_image_temp = $_FILES["image"]["tmp_name"];
+$ruta_a_subir = "images/$basename";
 
-echo $basename; 
-
+move_uploaded_file($url_image_temp, $ruta_a_subir);
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Response</title>
+</head>
+<body>
+
+    <img src="<?= $ruta_a_subir ?>" alt="css.png">
+    
+</body>
+</html>
